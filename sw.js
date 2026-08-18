1960502ed31db9d3ffaaacc42b53725dc782f018
Jesus Martinez Padron <jesusmartinezpadron@gmail.com>
2026-08-18 12:11:57 +0200

    fix(movil): menu superior legible, filtros compactos y ajustes limpios

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guarda la guia en el movil para que funcione sin cobertura en la sierra. */
-var CACHE = 'rupestre-v15';
+var CACHE = 'rupestre-v16';
 var ESENCIALES = ['./', './index.html', './manifest.webmanifest', './icono.svg'];
 
 self.addEventListener('install', function (ev) {
