3f77cb7d6164fa3824d9c9ad0f013058de582d3c
Jesus Martinez Padron <jesusmartinezpadron@gmail.com>
2026-08-18 11:47:03 +0200

    feat: rediseno movil - fichas plegables, tabla en tarjetas y botones tactiles

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guarda la guia en el movil para que funcione sin cobertura en la sierra. */
-var CACHE = 'rupestre-v7';
+var CACHE = 'rupestre-v8';
 var ESENCIALES = ['./', './index.html', './manifest.webmanifest', './icono.svg'];
 
 self.addEventListener('install', function (ev) {
