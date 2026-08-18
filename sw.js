bd47ab62a8393d1181c63f9b1395d25db63104e8
Jesus Martinez Padron <jesusmartinezpadron@gmail.com>
2026-08-18 11:41:18 +0200

    fix: sello de version legible tambien en la copia de trabajo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guarda la guia en el movil para que funcione sin cobertura en la sierra. */
-var CACHE = 'rupestre-v5';
+var CACHE = 'rupestre-v6';
 var ESENCIALES = ['./', './index.html', './manifest.webmanifest', './icono.svg'];
 
 self.addEventListener('install', function (ev) {
