6aea65cc5dc0596d7b30e93c79587bb023c19373
Jesus Martinez Padron <jesusmartinezpadron@gmail.com>
2026-08-18 11:38:58 +0200

    feat: aviso de version nueva y sello de actualizacion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guarda la guia en el movil para que funcione sin cobertura en la sierra. */
-var CACHE = 'rupestre-v3';
+var CACHE = 'rupestre-v4';
 var ESENCIALES = ['./', './index.html', './manifest.webmanifest', './icono.svg'];
 
 self.addEventListener('install', function (ev) {
