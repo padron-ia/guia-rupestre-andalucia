9e35ab913d7a239174ea40d6e9fd1665b8e3439b
Jesus Martinez Padron <jesusmartinezpadron@gmail.com>
2026-08-18 12:30:39 +0200

    feat(diseno): glifos con pigmento y controles en pastilla

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Guarda la guia en el movil para que funcione sin cobertura en la sierra. */
-var CACHE = 'rupestre-v18';
+var CACHE = 'rupestre-v19';
 var ESENCIALES = ['./', './index.html', './manifest.webmanifest', './icono.svg'];
 
 self.addEventListener('install', function (ev) {
